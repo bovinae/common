e4e40b352f5e2f7e94bce313ecc430267b0b2cc0
linwei <linwei@sics.ac.cn>
2023-04-12 16:09:47 +0800

    algorithm add compare any

--- algorithm/binary_search.go
+++ algorithm/binary_search.go
@@ -1,6 +1,11 @@
 package algorithm
 
-import "reflect"
+import (
+	"bytes"
+	"math"
+	"reflect"
+	"time"
+)
 
 const (
 	NOT_FOUND = -1
@@ -30,4 +35,92 @@ func BinarySearch(sortedSlice any, target any, compare func(i int, target any) i
 		left = mid + 1
 	}
 	return NOT_FOUND
+}
+
+func CompareAny(value1, value2 any) int {
+	if value1 == nil {
+		if value2 != nil {
+			return LESS
+		}
+		return EQUAL
+	}
+	if value2 == nil {
+		return GREATER
+	}
+	switch value1 := value1.(type) {
+	case []byte:
+		value2, ok := value2.([]byte)
+		if !ok {
+			return LESS
+		}
+		cmp := bytes.Compare(value1, value2)
+		if cmp < 0 {
+			return LESS
+		}
+		if cmp == 0 {
+			return EQUAL
+		}
+		return GREATER
+	case string:
+		value2, ok := value2.(string)
+		if !ok {
+			return LESS
+		}
+		if value1 < value2 {
+			return LESS
+		}
+		if value1 == value2 {
+			return EQUAL
+		}
+		return GREATER
+	case int64:
+		value2, ok := value2.(int64)
+		if !ok {
+			return LESS
+		}
+		if value1 < value2 {
+			return LESS
+		}
+		if value1 == value2 {
+			return EQUAL
+		}
+		return GREATER
+	case int:
+		value2, ok := value2.(int)
+		if !ok {
+			return LESS
+		}
+		if value1 < value2 {
+			return LESS
+		}
+		if value1 == value2 {
+			return EQUAL
+		}
+		return GREATER
+	case float64:
+		value2, ok := value2.(float64)
+		if !ok {
+			return LESS
+		}
+		if value1 < value2 {
+			return LESS
+		}
+		if math.Abs(value1-value2) < 0.000001 {
+			return EQUAL
+		}
+		return GREATER
+	case time.Time:
+		value2, ok := value2.(time.Time)
+		if !ok {
+			return LESS
+		}
+		if value1.Before(value2) {
+			return LESS
+		}
+		if value1.Equal(value2) {
+			return EQUAL
+		}
+		return GREATER
+	}
+	return LESS
 }
--- algorithm/binary_search_test.go
+++ algorithm/binary_search_test.go
@@ -30,3 +30,18 @@ func TestBinarySearch(t *testing.T) {
 		})
 	})
 }
+
+func TestCompareAny(t *testing.T) {
+	Convey("TestCompareAny", t, func() {
+		Convey("number", func() {
+			So(CompareAny(1, 7), ShouldEqual, LESS)
+			So(CompareAny(7, 7), ShouldEqual, EQUAL)
+			So(CompareAny(9, 7), ShouldEqual, GREATER)
+		})
+		Convey("string", func() {
+			So(CompareAny("1", "7"), ShouldEqual, LESS)
+			So(CompareAny("7", "7"), ShouldEqual, EQUAL)
+			So(CompareAny("9", "7"), ShouldEqual, GREATER)
+		})
+	})
+}
