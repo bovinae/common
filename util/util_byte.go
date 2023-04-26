bbdd0667e2c469524a87ac0bb53abd3461c735c1
linwei <linwei@sics.ac.cn>
2023-04-26 11:57:47 +0800

    add trie

--- util/util_byte.go
+++ util/util_byte.go
@@ -1,6 +1,6 @@
 package util
 
-func GetPrefix(prefix, curr []byte) []byte {
+func GetPrefix(prefix, curr []rune) []rune {
 	i, j := 0, 0
 	for i < len(prefix) && j < len(curr) {
 		if prefix[i] != curr[j] {
@@ -12,7 +12,7 @@ func GetPrefix(prefix, curr []byte) []byte {
 	return curr[:i]
 }
 
-func GetSuffix(suffix, curr []byte) []byte {
+func GetSuffix(suffix, curr []rune) []rune {
 	i, j := len(suffix)-1, len(curr)-1
 	for i >= 0 && j >= 0 {
 		if suffix[i] != curr[j] {
--- util/util_byte_test.go
+++ util/util_byte_test.go
@@ -9,10 +9,10 @@ import (
 func TestGetPrefix(t *testing.T) {
 	Convey("TestGetPrefix", t, func() {
 		Convey("chinese", func() {
-			So(GetPrefix([]byte("找第一个1非空元素"), []byte("找第一个123")), ShouldResemble, []byte("找第一个1"))
+			So(GetPrefix([]rune("找第一个1非空元素"), []rune("找第一个123")), ShouldResemble, []rune("找第一个1"))
 		})
 		Convey("english", func() {
-			So(GetPrefix([]byte("english"), []byte("engilish")), ShouldResemble, []byte("eng"))
+			So(GetPrefix([]rune("english"), []rune("engilish")), ShouldResemble, []rune("eng"))
 		})
 	})
 }
@@ -20,10 +20,10 @@ func TestGetPrefix(t *testing.T) {
 func TestGetSuffix(t *testing.T) {
 	Convey("TestGetSuffix", t, func() {
 		Convey("chinese", func() {
-			So(GetSuffix([]byte("找第一个1非空元素"), []byte("找第一个123")), ShouldResemble, []byte(""))
+			So(GetSuffix([]rune("找第一个1非空元素"), []rune("找第一个123")), ShouldResemble, []rune(""))
 		})
 		Convey("english", func() {
-			So(GetSuffix([]byte("english"), []byte("engilish")), ShouldResemble, []byte("lish"))
+			So(GetSuffix([]rune("english"), []rune("engilish")), ShouldResemble, []rune("lish"))
 		})
 	})
 }
