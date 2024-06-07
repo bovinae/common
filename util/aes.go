749412aacebc3628a5233068114625e3bc43e9d2
linwei <linwei@sics.ac.cn>
2024-06-07 16:56:28 +0800

    aes

--- util/aes.go
+++ util/aes.go
@@ -6,28 +6,36 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"io"
+
+	"github.com/pkg/errors"
 )
 
 // =================== CBC ======================
-func AesEncryptCBC(origData []byte, key []byte) (encrypted []byte) {
+func AesEncryptCBC(origData []byte, key []byte) (encrypted []byte, err error) {
 	// 分组秘钥
 	// NewCipher该函数限制了输入k的长度必须为16, 24或者32
-	block, _ := aes.NewCipher(key)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	origData = pkcs5Padding(origData, blockSize)                // 补全码
 	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) // 加密模式
 	encrypted = make([]byte, len(origData))                     // 创建数组
 	blockMode.CryptBlocks(encrypted, origData)                  // 加密
-	return encrypted
+	return encrypted, nil
 }
-func AesDecryptCBC(encrypted []byte, key []byte) (decrypted []byte) {
-	block, _ := aes.NewCipher(key)                              // 分组秘钥
+func AesDecryptCBC(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	block, err := aes.NewCipher(key) // 分组秘钥
+	if err != nil {
+		return nil, err
+	}
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) // 加密模式
 	decrypted = make([]byte, len(encrypted))                    // 创建数组
 	blockMode.CryptBlocks(decrypted, encrypted)                 // 解密
 	decrypted = pkcs5UnPadding(decrypted)                       // 去除补全码
-	return decrypted
+	return decrypted, nil
 }
 func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
 	padding := blockSize - len(ciphertext)%blockSize
@@ -41,8 +49,11 @@ func pkcs5UnPadding(origData []byte) []byte {
 }
 
 // =================== ECB ======================
-func AesEncryptECB(origData []byte, key []byte) (encrypted []byte) {
-	cipher, _ := aes.NewCipher(generateKey(key))
+func AesEncryptECB(origData []byte, key []byte) (encrypted []byte, err error) {
+	cipher, err := aes.NewCipher(generateKey(key))
+	if err != nil {
+		return nil, err
+	}
 	length := (len(origData) + aes.BlockSize) / aes.BlockSize
 	plain := make([]byte, length*aes.BlockSize)
 	copy(plain, origData)
@@ -56,12 +67,14 @@ func AesEncryptECB(origData []byte, key []byte) (encrypted []byte) {
 		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
 	}
 
-	return encrypted
+	return encrypted, nil
 }
-func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte) {
-	cipher, _ := aes.NewCipher(generateKey(key))
+func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	cipher, err := aes.NewCipher(generateKey(key))
+	if err != nil {
+		return nil, err
+	}
 	decrypted = make([]byte, len(encrypted))
-	//
 	for bs, be := 0, cipher.BlockSize(); bs < len(encrypted); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
 		cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
 	}
@@ -71,7 +84,7 @@ func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte) {
 		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
 	}
 
-	return decrypted[:trim]
+	return decrypted[:trim], nil
 }
 func generateKey(key []byte) (genKey []byte) {
 	genKey = make([]byte, 16)
@@ -85,29 +98,32 @@ func generateKey(key []byte) (genKey []byte) {
 }
 
 // =================== CFB ======================
-func AesEncryptCFB(origData []byte, key []byte) (encrypted []byte) {
+func AesEncryptCFB(origData []byte, key []byte) (encrypted []byte, err error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	encrypted = make([]byte, aes.BlockSize+len(origData))
 	iv := encrypted[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+		return nil, err
 	}
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(encrypted[aes.BlockSize:], origData)
-	return encrypted
+	return encrypted, nil
 }
-func AesDecryptCFB(encrypted []byte, key []byte) (decrypted []byte) {
-	block, _ := aes.NewCipher(key)
+func AesDecryptCFB(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 	if len(encrypted) < aes.BlockSize {
-		panic("ciphertext too short")
+		return nil, errors.New("ciphertext too short")
 	}
 	iv := encrypted[:aes.BlockSize]
 	encrypted = encrypted[aes.BlockSize:]
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 	stream.XORKeyStream(encrypted, encrypted)
-	return encrypted
+	return encrypted, nil
 }
--- util/aes_test.go
+++ util/aes_test.go
@@ -1,10 +1,10 @@
 package util
 
 import (
-	"encoding/base64"
-	"encoding/hex"
 	"log"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestAes(t *testing.T) {
@@ -12,24 +12,29 @@ func TestAes(t *testing.T) {
 	key := []byte("ABCDEFGHIJKLMNOP") // 加密的密钥
 	log.Println("原文：", string(origData))
 
-	log.Println("------------------ CBC模式 --------------------")
-	encrypted := AesEncryptCBC(origData, key)
-	log.Println("密文(hex): ", hex.EncodeToString(encrypted))
-	log.Println("密文(base64): ", base64.StdEncoding.EncodeToString(encrypted))
-	decrypted := AesDecryptCBC(encrypted, key)
-	log.Println("解密结果：", string(decrypted))
-
-	log.Println("------------------ ECB模式 --------------------")
-	encrypted = AesEncryptECB(origData, key)
-	log.Println("密文(hex): ", hex.EncodeToString(encrypted))
-	log.Println("密文(base64): ", base64.StdEncoding.EncodeToString(encrypted))
-	decrypted = AesDecryptECB(encrypted, key)
-	log.Println("解密结果：", string(decrypted))
-
-	log.Println("------------------ CFB模式 --------------------")
-	encrypted = AesEncryptCFB(origData, key)
-	log.Println("密文(hex): ", hex.EncodeToString(encrypted))
-	log.Println("密文(base64): ", base64.StdEncoding.EncodeToString(encrypted))
-	decrypted = AesDecryptCFB(encrypted, key)
-	log.Println("解密结果：", string(decrypted))
+	Convey("TestAes", t, func() {
+		Convey("------------------ CBC模式 --------------------", func() {
+			encrypted, err := AesEncryptCBC(origData, key)
+			So(err, ShouldBeNil)
+			// log.Println("密文(hex): ", hex.EncodeToString(encrypted))
+			// log.Println("密文(base64): ", base64.StdEncoding.EncodeToString(encrypted))
+			decrypted, err := AesDecryptCBC(encrypted, key)
+			So(err, ShouldBeNil)
+			So(decrypted, ShouldResemble, origData)
+		})
+		Convey("------------------ ECB模式 --------------------", func() {
+			encrypted, err := AesEncryptECB(origData, key)
+			So(err, ShouldBeNil)
+			decrypted, err := AesDecryptECB(encrypted, key)
+			So(err, ShouldBeNil)
+			So(decrypted, ShouldResemble, origData)
+		})
+		Convey("------------------ CFB模式 --------------------", func() {
+			encrypted, err := AesEncryptCFB(origData, key)
+			So(err, ShouldBeNil)
+			decrypted, err := AesDecryptCFB(encrypted, key)
+			So(err, ShouldBeNil)
+			So(decrypted, ShouldResemble, origData)
+		})
+	})
 }
